e5e7e46a68473e5c4d64e473aa69170b0f933897
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2024-01-11 11:10:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ const result = document.getElementById('weatherData')
 btn.onclick = ()=>{
 	fetch(`https://api.openweathermap.org/data/2.5/weather?q=london&appid=7d8db3a49de7c4affd734cb6dfcab991`)
 	.then(data=>data.json())
-	.then(data=>{
-		console.log(data)
-	result.innerText = "Current weather in London: "+data.weather[0].main
+	.then(response=>{
+	result.innerText = `Current weather in London: ${response.weather[0].main}`
 	})
 }
